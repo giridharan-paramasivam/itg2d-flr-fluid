d699be8029ffe148f647817efd93f93e0f4a7d41
Giridharan Paramasivam <rpgiridharan@gmail.com>
2026-08-26 18:34:31 +0200

    removed redundant save_data from gensolver file

--- itg2d.py
+++ itg2d.py
@@ -6,7 +6,8 @@ import h5py as h5
 from modules.mlsarray import Slicelist,init_kgrid
 from modules.mlsarray import irft2 as original_irft2, rft2 as original_rft2, irft as original_irft, rft as original_rft
 from modules.gamma import gam_max   
-from modules.gensolver import Gensolver,save_data
+from modules.gensolver import Gensolver
+from modules.h5tools import save_data
 from functools import partial
 from modules.basics import format_exp, round_to_nsig
 import os
--- modules/gensolver.py
+++ modules/gensolver.py
@@ -1,37 +1,6 @@
 from time import time
 import numpy as np
 
-def save_data(fl,grpname,ext_flag,**kwargs):
-    if not (grpname in fl):
-        grp=fl.create_group(grpname)
-    else:
-        grp=fl[grpname]
-    for l,m in kwargs.items():
-        if not l in grp:
-            if(not ext_flag):
-                grp[l]=m
-            else:
-                if(np.isscalar(m)):
-                    grp.create_dataset(l,(1,),maxshape=(None,),dtype=type(m))
-                    if(not fl.swmr_mode):
-                        fl.swmr_mode = True
-                else:
-                    grp.create_dataset(l,(1,)+m.shape,chunks=(1,)+m.shape,maxshape=(None,)+m.shape,dtype=m.dtype)
-                    if(not fl.swmr_mode):
-                        fl.swmr_mode = True
-                lptr=grp[l]
-                lptr[-1,]=m
-                lptr.flush()
-        else:
-            lptr=grp[l]
-            if(ext_flag):
-                lptr.resize((lptr.shape[0]+1,)+lptr.shape[1:])
-                lptr[-1,]=m
-            else:
-                lptr[...]=m
-            lptr.flush()
-        fl.flush()
-
 class Gensolver:    
     def __init__(self,solver,f,t0,y0,t1,fsave,fshow=None,fy=None,dtstep=0.1,dtshow=None,dtsave=None,dtfupdate=None,force_update=None,dense=False,**kwargs):
 
